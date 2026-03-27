ff4a5c8deb89a40ff913fa4404418cf3df989c7e
ArielMariussi <arielmariussi@gmail.com>
2026-03-26 22:05:33 -0300

    rebuild com alteracoes

--- obj/Release/net8.0/MeuPortfolio.AssemblyInfo.cs
+++ obj/Release/net8.0/MeuPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b32afb71c98683335e64316a9680cc47d87ebf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b26fa1ce946a5bc492e648fba93a60d0822f010")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
